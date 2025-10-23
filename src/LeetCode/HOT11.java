0f618ad35671f67e11bd56de350c8e6bc5699820
ZH0531 <ZH0531W@Outlook.com>
2025-10-23 20:24:37 +0800

    LeetCode-2025/10/23

---
+++ src/LeetCode/HOT11.java
@@ -0,0 +1,42 @@
+package LeetCode;
+
+public class HOT11 {
+    /**
+     * 给定一个长度为 的整数数组 。 有 条垂线，第 条线的两个端点是 和 。nheightni(i, 0)(i, height[i])
+     * <p>
+     * 找出其中的两条线，使得它们与  轴共同构成的容器可以容纳最多的水。x
+     * <p>
+     * 返回容器可以储存的最大水量。
+     * <p>
+     * 说明：你不能倾斜容器。
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：[1,8,6,2,5,4,8,3,7]
+     * 输出：49
+     * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+     * 示例 2：
+     * 输入：height = [1,1]
+     * 输出：1
+     * <p>
+     * 提示：
+     * n == height.length
+     * 2 <= n <= 105
+     * 0 <= height[i] <= 104
+     */
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int max = 0;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            max = Math.max(max, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+}
